d0db10b67d5b1be4855701320f6b11625132040d
Fellipe Coelho <fontan.fellipe@gmail.com>
2021-03-20 14:14:21 -0300

    created pessoa class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections
+
+class Pessoa {
+    var name: String = "Fellipe"
+    var cpf : String = "0000000000"
+}
+
+fun main(){
+    val fellipe = Pessoa()
+
+    print(fellipe.name)
+}
